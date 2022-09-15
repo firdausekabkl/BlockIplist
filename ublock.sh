1aa2e54d6bd01d119f26f008ae87e07246b97915
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2022-09-15 19:15:06 +0700

    Update ublock.sh

--- ublock.sh
+++ ublock.sh
@@ -1,1 +1,15 @@
+chmod +x IPupdate.sh
+chmod +x ublock.sh
 
+
+./IPupdate.sh
+
+#!/bin/bash
+for i in $(cat u-ip.txt);
+do
+sudo ufw reject from "$i" to any
+done
+
+#the rules & Reload Services
+service iptables save
+sudo ufw reload
