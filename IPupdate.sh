6f0f3f0381636690271c5a87f02900e493dd40cd
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2022-09-15 19:40:37 +0700

    Update IPupdate.sh

--- IPupdate.sh
+++ IPupdate.sh
@@ -2,8 +2,8 @@
 sudo rm b-ip.txt
 sudo rm u-ip.txt
 
-wget https://github.com/firdausekabkl/BlockIplist/blob/main/b-ip.txt
-wget https://github.com/firdausekabkl/BlockIplist/blob/main/u-ip.txt
+wget https://estory.my.id/b-ip.txt
+wget https://estory.my.id/u-ip.txt
 
 chmod +x b-ip.txt
 chmod +x u-ip.txt
