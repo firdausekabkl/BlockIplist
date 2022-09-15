ee15ac07bba80d0bea9fe2c3182eaea449864ab0
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2022-09-15 19:54:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,12 @@ chmod +x ublock.sh
 #!/bin/bash
 for b in $(cat b-ip.txt);
 do
+sudo firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address='"$b"' reject"
+sudo iptables -I INPUT -s $b -j DROP
 sudo ufw reject from "$b" to any
 done
 
 #the rules & Reload Services
 service iptables save
 sudo ufw reload
+sudo firewall-cmd --reload
