53c8a0dc062eb74e14adaa62e555a2ca84cb0db6
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2022-09-16 08:13:49 +0700

    Update IPupdate.sh

--- IPupdate.sh
+++ IPupdate.sh
@@ -2,8 +2,8 @@ mytitle="Update List IP"
 sudo rm b-ip.txt
 sudo rm u-ip.txt
 
-wget https://datafirdausbkl.web.app/b-ip.txt
-wget https://datafirdausbkl.web.app/u-ip.txt
+curl https://datafirdausbkl.web.app/b-ip.txt >> b-ip.txt
+curl https://datafirdausbkl.web.app/u-ip.txt >> u-ip.txt
 
 chmod +x b-ip.txt
 chmod +x u-ip.txt
