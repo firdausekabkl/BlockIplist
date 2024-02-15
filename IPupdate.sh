55067e3e7f6cf80405be200d0e079892f9b80b48
firdausekabkl <36780967+firdausekabkl@users.noreply.github.com>
2024-02-15 19:19:01 +0700

    Update IPupdate.sh

--- IPupdate.sh
+++ IPupdate.sh
@@ -2,6 +2,7 @@ mytitle="Update List IP"
 sudo rm b-ip.txt
 sudo rm u-ip.txt
 
+echo Ubah Sesuai dengan tempat anda menyimpan data IP, Bisa juga Mengunakan AbuseIPDB
 curl https://datafirdausbkl.web.app/b-ip.txt >> b-ip.txt
 curl https://datafirdausbkl.web.app/u-ip.txt >> u-ip.txt
 
